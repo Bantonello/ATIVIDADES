d5b7b8f3f7420c9d85e4a70d715c936560cf57b2
Bruna Costa <bantonellodj@gmail.com>
2022-05-29 13:31:25 -0300

    Atualizando

--- EXCERCICIOS HTML E CSS/Hora do dia/script.js
+++ EXCERCICIOS HTML E CSS/Hora do dia/script.js
@@ -1,20 +1,21 @@
 function carregar() {
   let mensagem = document.getElementById("container-msg");
-  let imagem = document.getElementById("conteiner-img");
+  let imagem = document.getElementById("img");
 
   let data = new Date();
-  let hora = 19;
+  let hora = data.getHours();
+  //let hora = 19;
 
   mensagem.innerHTML = `Agora são ${hora} horas`;
 
   if (hora > 0 && hora <= 12) {
-    imagem.src = "./assets/manha.jpeg";
+    imagem.src = "manha.jpeg";
     document.body.style.background = "#D3C6A4";
   } else if (hora > 12 && hora <= 18) {
-    imagem.src = "./assets/tarde.jpeg";
+    imagem.src = "tarde.jpeg";
     document.body.style.background = "#FD8504";
   } else if (hora > 18) {
-    imagem.src = "./assets/noite.jpeg";
+    imagem.src = "noite.jpeg";
     document.body.style.background = "#0067A4";
   }
 }
